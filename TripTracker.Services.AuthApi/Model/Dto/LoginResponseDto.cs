29d4128a342502e5cbfd63750a1220ace985bc10
ntraja11 <ntraja11@gmail.com>
2025-05-05 23:42:39 +0200

    Necessary Dtos added to AuthApi

---
+++ TripTracker.Services.AuthApi/Model/Dto/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TripTracker.Services.AuthApi.Model.Dto
+{
+    public class LoginResponseDto
+    {
+        public string? Token { get; set; }
+        public UserDto? User { get; set; }
+    }
+}
---
+++ TripTracker.Services.AuthApi/Model/Dto/RegistrationRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TripTracker.Services.AuthApi.Model.Dto
+{
+    public class RegistrationRequestDto
+    {
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
